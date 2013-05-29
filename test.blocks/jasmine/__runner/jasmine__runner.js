c2980bb6f339e898ad45cf7d835b06224b5d624d
Misha Berezin <mishaberezin@gmail.com>
2013-05-30 01:21:41 +0300

    Убрал зависимость от jQuery.

--- test.blocks/jasmine/__runner/jasmine__runner.js
+++ test.blocks/jasmine/__runner/jasmine__runner.js
@@ -1,12 +1,25 @@
-$(window).load(function() {
+(function() {
 
-    jasmine._reporterInstance = new jasmine.HtmlReporter();
+    listen(window, 'load', function() {
 
-    setTimeout(
-        function() {
-            jasmine.getEnv().addReporter(jasmine._reporterInstance);
-            jasmine.getEnv().execute();
-        },
-        10);
+        jasmine._reporterInstance = new jasmine.HtmlReporter();
 
-});
+        setTimeout(
+            function() {
+                jasmine.getEnv().addReporter(jasmine._reporterInstance);
+                jasmine.getEnv().execute();
+            },
+            10);
+
+    });
+
+    function listen(elem, evnt, callback) {
+        if(elem.addEventListener) {
+            elem.addEventListener(evnt, callback, false);
+        }
+        else if(elem.attachEvent) {
+            elem.attachEvent("on" + evnt, callback);
+        }
+    };
+
+})();
