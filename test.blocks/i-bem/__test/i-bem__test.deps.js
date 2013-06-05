e5dda2d24ea27f929888f7e8717dce4f83ce86c3
Misha Berezin <mishaberezin@gmail.com>
2013-06-05 17:01:03 +0300

    Убрал Jasmine, прикрутил mocha+chai. Переименовал технологию "phantom.js" в "phantomjs". Запуск тестов в консоли произвожу через mocha-phantomjs. Технология "phantomjs" больше не продуцирует файлов, полезный выхлоп выражается только в запуске тестов черзе mocha-phantomjs.

--- test.blocks/i-bem/__test/i-bem__test.deps.js
+++ test.blocks/i-bem/__test/i-bem__test.deps.js
@@ -2,6 +2,6 @@
     mustDeps: { block: 'i-bem', elem: 'dom' },
     shouldDeps: [
         { block: 'i-ecma', elems: ['array', 'object'] },
-        { block: 'jasmine' }
+        { block: 'mocha' }
     ]
 })
