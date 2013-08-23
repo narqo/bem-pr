3cffa864c0e29b4baf1fe3e08eecc51ed1bc1e34
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-08-23 17:09:28 +0400

    Code style refactored

--- bem/levels/sets.js
+++ bem/levels/sets.js
@@ -1,15 +1,13 @@
 var PATH = require('path'),
     PRJ_TECHS = PATH.resolve(__dirname, '../techs'),
-    join = PATH.join.bind(null, PRJ_TECHS);
+    resolveTech = PATH.join.bind(null, PRJ_TECHS);
 
 exports.baseLevelPath = require.resolve('bem/lib/levels/simple');
 
 exports.getTechs = function() {
-
     return {
-        'sets'      : join('sets.js'),
-        'examples'  : join('examples.js'),
-        'tests'     : join('tests.js')
+        'sets'      : resolveTech('sets.js'),
+        'examples'  : resolveTech('examples.js'),
+        'tests'     : resolveTech('tests.js')
     };
-
 };
