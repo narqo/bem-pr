11ce7f1a53a0150ef83e63b93788ce34d8718cdb
Vladimir Varankin <nek.narqo+git@gmail.com>
2013-08-23 18:26:00 +0400

    test-tmpl module should store declarations in 'decl' key of test block
    Drop dependency from "page" block

--- bem/techs/test-tmpl.js
+++ bem/techs/test-tmpl.js
@@ -2,24 +2,24 @@ var PATH = require('path'),
     tmpl = require('bem/lib/template');
 
 exports.API_VER = 2;
+
 exports.techMixin = {
 
     getCreateResult : function(path, suffix, vars) {
-
         var envProps = JSON.parse(process.env.__tests || '{}')[PATH.dirname(path)] || {};
 
         return tmpl.process([
-            '({',
-            '    block: "page",',
-            '    head: [',
-            '        { elem: "css", url: "_{{bemBundleName}}.css", ie: false },',
-            '        { elem: "js", url: "_{{bemBundleName}}.test.js" }',
-            '    ],',
-            '    content: {',
-            '        block: "test",',
-            '        content: {{bemTmplContent}}',
+            '({ tag: \'html\', content: [',
+            '  { tag: \'head\', content: [',
+            '    { tag: \'link\', attrs: { href: \'_{{bemBundleName}}.css\' } },',
+            '    { tag: \'script\', attrs: { src: \'_{{bemBundleName}}.test.js\' } }',
+            '  ] },',
+            '  { tag: \'body\', content:',
+            '    { block: \'test\', decl:',
+            '      {{bemTmplContent}}',
             '    }',
-            '})'
+            '  }',
+            ']})'
         ], {
             BundleName: envProps.BundleName || vars.BlockName,
             TmplContent: envProps.TmplContent || ''
